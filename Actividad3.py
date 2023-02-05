9f3b32c6f860121f92e586e952b30942f266229d
IDGS-804-19002061 <19002061@alumnos.utleon.edu.mx>
2023-02-05 17:29:50 -0600

    Cinepolis Actividad 03/02/2023

--- Actividad3.py
+++ Actividad3.py
@@ -1,42 +1,53 @@
 from flask import Flask, render_template, request
-import math
+
+class Pago:
+        
+        def __init__(self, precio, boletos, compradores):
+                self.precio = int(precio)
+                self.boletos = int(boletos)
+                self.compradores = int(compradores)
+                self.max = self.boletos/self.compradores
+                self.total = self.precio * self.boletos
+
+        def descuento(self):
+            if self.boletos>5:
+                self.total = self.total*0.85
+            elif self.boletos>=3:
+                self.total = self.total*0.90
+            else:
+                self.total = self.total
+
+        def descuentoTarjeta(self, tarjeta):
+            if tarjeta=="s":
+                self.total = self.total*0.90
+            else:
+                self.total = self.total
 
 app = Flask(__name__)
 
 @app.route("/act3")
 def func():
-
     return render_template("act3.html")
+    
 
 @app.route("/pago", methods=["post"])
-def multi():
+def pago():
+    pago = Pago(
+        12000, 
+        int(request.form.get("boletos")),
+        int(request.form.get("compradores"))
+        )
 
-    if request.method=="post":
-        precio=12000
-        nombre=request.form.get("nombre")
-        compradores=int(request.form.get("compradores"))
-        tarjeta=request.form.get("card")
-        boletos=int(request.form.get("boletos"))
-        max = boletos/compradores
+    tarjeta = request.form.get("tarjeta")
+
+    if pago.max >7:
+        return render_template("error.html")
+    else:
+        pago.descuento()
+        pago.descuentoTarjeta(tarjeta)
+        return render_template("pago.html", res=pago.total)
+
     
-        if max >= 7:
-        
-            res = precio*boletos
-            if boletos>5:
-                res = res*0.85
-            elif boletos>=3:
-                res = res*0.90
-            else:
-                res = res
-
-            if tarjeta=="s":
-                res = res*0.90
-        else:
-            return '''
-            <h1>Se ha alcanzado el numero maximo de boletos por comprador</h1>
-            '''
-
-    return render_template("pago.html", res=res, nombre=nombre, compradores=compradores, tarjeta=tarjeta, boletos=boletos)
 
 
 if __name__ == "__main__":
